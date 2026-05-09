84ef9e3da29bedd95455d754c2662ee9379be753
RadoslawSmoronski <radoslaw.smo@gmail.com>
2026-05-09 21:36:27 +0200

    frontend: create main website (#10)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,14 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import ChatPage from './pages/ChatPage'
+import HomePage from './pages/HomePage'
 
 export default function App() {
-  return <ChatPage />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/chat" element={<ChatPage />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
